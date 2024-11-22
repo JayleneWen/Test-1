baa54fe45ae33a1c8da2346c1c97c56a87cc3ff0
Jaylene <jwjx2018@gmail.com>
2024-11-22 16:54:43 -0500

    hii

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
-print("helloWorld")
-print("Bye")
+for i in range (5):
+ print(i)
+print()
+for i in range (5,18,3):
+  print(i)
+print()
+for i in range (0,35,5):
+  print(i)
+print()
+for i in range (1500,1700):
+  if i % 7 == 0 and i % 5 == 0:
+    print(i)
+
+ 
+  
